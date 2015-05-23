4ec96d02a7f5a830d437e87d8e322aa6d02a86f1
Dotan Nahum <jondotan@gmail.com>
2015-05-23 21:25:17 +0300

    spf

---
+++ spf13/setup.sh
@@ -0,0 +1,5 @@
+cd ~
+ln -s dotfiles/spf13/.gvimrc .gvimrc
+ln -s dotfiles/spf13/.vimrc.before.local .vimrc.before.local
+ln -s dotfiles/spf13/.vimrc.bundles.local .vimrc.bundles.local
+ln -s dotfiles/spf13/.vimrc.local .vimrc.local
