830563bdf4045a3ce1ed7ec5f5a42b1a811cdad2
Dotan Nahum <jondotan@gmail.com>
2015-09-20 16:32:32 +0300

    ycm setup

--- spf13/setup.sh
+++ spf13/setup.sh
@@ -3,3 +3,7 @@ ln -s dotfiles/spf13/.gvimrc .gvimrc
 ln -s dotfiles/spf13/.vimrc.before.local .vimrc.before.local
 ln -s dotfiles/spf13/.vimrc.bundles.local .vimrc.bundles.local
 ln -s dotfiles/spf13/.vimrc.local .vimrc.local
+
+echo installing YCM
+./.vim/bundle/YouCompleteMe/install.py --gocode-completer --clang-completer
+
